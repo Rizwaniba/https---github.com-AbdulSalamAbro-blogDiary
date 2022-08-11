4ac310f0452e383d866dd388cca652988dacf635
abdulsalam <abdulsalam.ksef18@iba-suk.edu.pk>
2022-08-11 21:20:44 +0900

    first commit in backend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
