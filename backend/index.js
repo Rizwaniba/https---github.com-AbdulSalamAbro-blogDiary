f7b2868d3c14f31e0c9cd48607a2cc63355f3f23
abdulsalam <abdulsalam.ksef18@iba-suk.edu.pk>
2022-08-11 21:24:57 +0900

    second commit

--- backend/index.js
+++ backend/index.js
@@ -12,3 +12,16 @@ const app = express();
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+
+const CONNECTION_URL = 'mongodb://localhost:27017';;
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
