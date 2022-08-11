45dc40c6ab9fab34cf6930ad3f6005adac0f4b0e
abdulsalam <abdulsalam.ksef18@iba-suk.edu.pk>
2022-08-12 02:46:01 +0900

    new folder added for models

--- models/user.js
+++ models/user.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
+
+//here we are designing the database model for our sign up page in which we will define the fields 
+
 const userSchema = mongoose.Schema({
   name: { type: String, required:  true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  
   id: { type: String },
 });
 
